755fbe857cfd4eef78cbadc9ce9839457a355712
helsonxiao <helsonxiao@gmail.com>
2019-11-09 20:46:55 +0800

    play chess together

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -15,7 +15,6 @@
       this.boardTask = setTimeout(() => {
         this.ctx = uni.createCanvasContext('myCanvas');
         this.designBoard();
-        this.designChess(0, 0);
         this.ctx.draw();
       }, 100)
 
@@ -23,7 +22,7 @@
       this.isMe = true;
       this.chessBox = [];
       for (let i = 0; i < this.rowCount; i += 1) {
-        this.chessBox[i] = []
+        this.chessBox[i] = [];
         for (let j = 0; j < this.columnCount; j += 1) {
           this.chessBox[i][j] = 0;
         }
@@ -45,12 +44,13 @@
         this.ctx.stroke();
       },
       designChess(x, y) {
-        // if (this.chessBox[x][y] !== 0) {
-        //   return;
-        // }
+        if (this.chessBox[x][y] !== 0) {
+          return;
+        }
 
-        // this.chessBox[x][y] = this.isMe ? 1 : 2;
-        // this.isMe = !this.isMe;
+        this.chessBox[x][y] = this.isMe ? 1 : 2;
+        this.isMe = !this.isMe;
+        
         this.ctx.beginPath();
         this.ctx.arc(
           (x + 1) * this.padding,
@@ -60,7 +60,9 @@
           2 * Math.PI,
         );
         this.ctx.closePath();
-        // this.ctx.fillStyle = this.chessBox[x][y] === 1 ? 'black' : 'red';
+        
+        this.ctx.fillStyle = this.chessBox[x][y] === 1 ? 'black' : 'red';
+        
         this.ctx.fill();
       },
       handleTouchEnd(event) {
